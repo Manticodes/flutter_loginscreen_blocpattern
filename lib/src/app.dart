cd4f222567bb1724bf1da35f253c5100e893f2de
Sina Zerehpoosh <sina.zrp@gmail.com>
2022-07-27 14:48:23 +0430

    setup project from scratch

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_loginscreen_blocpattern/src/screens/loginscreen.dart';
+
+class App extends StatelessWidget {
+  build(context) => MaterialApp(
+        title: 'Log me in ',
+        home: LoginScreen(),
+      );
+}
